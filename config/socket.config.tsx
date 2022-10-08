5af8dd1114a472cd6fa4b97f46bca2df8e1b6e3e
tonylee221b <blackcoder787@gmail.com>
2022-10-08 12:21:11 -0400

    config changed

--- config/socket.config.tsx
+++ config/socket.config.tsx
@@ -1,2 +1,2 @@
-// export const SOCKET_URL = 'https://pop-chat-socket-server.herokuapp.com/'
-export const SOCKET_URL = 'http://localhost:3001'
+export const SOCKET_URL = 'https://pop-chat-socket-server.herokuapp.com/'
+// export const SOCKET_URL = 'http://localhost:3001'
