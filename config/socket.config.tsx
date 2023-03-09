ff9383c000b0723cd9ca28117d8fc258ab0c294e
tonylee221b <blackcoder787@gmail.com>
2023-03-09 00:41:18 -0800

    netlify => vercel

--- config/socket.config.tsx
+++ config/socket.config.tsx
@@ -1,2 +1,3 @@
-export const SOCKET_URL = 'https://main--pop-chat-server.netlify.app/'
+export const SOCKET_URL =
+  'https://pop-chat-server-3ue87oyzs-tonylee221b.vercel.app/'
 // export const SOCKET_URL = 'http://localhost:3001'
