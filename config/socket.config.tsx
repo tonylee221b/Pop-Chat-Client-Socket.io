d477eac8666264844d6af5921752c6890428f967
tonylee221b <blackcoder787@gmail.com>
2023-03-08 23:55:43 -0800

    heroku => netlify

--- config/socket.config.tsx
+++ config/socket.config.tsx
@@ -1,2 +1,2 @@
-export const SOCKET_URL = 'https://pop-chat-socket-server.herokuapp.com/'
+export const SOCKET_URL = 'https://main--pop-chat-server.netlify.app/'
 // export const SOCKET_URL = 'http://localhost:3001'
